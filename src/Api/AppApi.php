c92c58881d17a0e2a02bb4da66eaa4f49053a138
mx <oscarmx0912@outlook.com>
2023-07-12 18:47:42 +0800

    Update AppApi.php

--- src/Api/AppApi.php
+++ src/Api/AppApi.php
@@ -5,7 +5,7 @@ namespace UUAI\Sdk\Api;
 use UUAI\Sdk\Entity\UserAccessTokenRequest;
 use UUAI\Sdk\Entity\UserInfoRequest;
 
-class AppApi extends OpenApi
+class AppApi extends BaseApi
 {
     const API_APP_INFO = '/open/apis/app/plan';
 
